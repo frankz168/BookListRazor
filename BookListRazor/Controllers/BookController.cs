9a35a033a0ced3e003722c6f27a8a010c5d442ff
frankz168 <franky.sutanto93@gmail.com>
2022-07-12 23:26:17 +0700

    first commit

---
+++ BookListRazor/Controllers/BookController.cs
@@ -0,0 +1,45 @@
+﻿using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Controllers
+{
+    [Route("api/Book")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBookAll()
+        {
+            return Json(new { data = _db.Book.ToList() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDB = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookFromDB == null)
+                return Json(new { success = false, message = "Error while Deleting" });
+
+            _db.Book.Remove(bookFromDB);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successfully" });
+        }
+    }
+}
---
+++ BookListRazor/Model/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    // the ApplicationDbContext use for create table in my sql server automatically from here
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set;}
+    }
+}
